98978c149eef5c05b7881c07dc78fa0e9f7fae47
Claus Prüfer <pruefer@webcodex.de>
2024-07-07 12:50:59 +0200

    initial import of /lib (libraries).

---
+++ lib/http/httpnet.cpp
@@ -0,0 +1,15 @@
+#include "httpnet.hpp"
+
+using namespace std;
+
+
+HTTPNet::HTTPNet()
+{
+    DBG(120, "Constructor");
+}
+
+HTTPNet::~HTTPNet()
+{
+    DBG(120, "Destructor");
+}
+
---
+++ lib/http/httpnet.hpp
@@ -0,0 +1,26 @@
+#ifndef LibHTTP_net_hpp
+#define LibHTTP_net_hpp
+
+#include <boost/regex.hpp>
+
+#include "../../Debug.cpp"
+#include "../../Helper.hpp"
+
+#include <string>
+
+
+class HTTPNet
+{
+
+public:
+
+    HTTPNet();
+    ~HTTPNet();
+
+private:
+
+protected:
+
+};
+
+#endif
