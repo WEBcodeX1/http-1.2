70b9f215d20a0bc7a89cf729324724419b42dae6
Claus Prüfer <pruefer@webcodex.de>
2024-10-29 20:47:45 +0100

    Adapt working config Closes #91

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'HTTP1.2'
+project = 'falconas'
 copyright = '2024, Claus Prüfer'
 author = 'Claus Prüfer'
 
@@ -43,12 +43,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'rtd'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-breathe_default_project = "HTTP1.2"
+breathe_default_project = "falconas"
 
--- doc/index.rst
+++ doc/index.rst
@@ -3,13 +3,15 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to HTTP/1.2's documentation!
-====================================
+FalconAS (HTTP Python Application Server) Documentation.
+========================================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+.. doxygenclass:: ClientThread
+   :members:
 
 
 Indices and tables
