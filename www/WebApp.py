f0923f4427941b64483594abf29549b5f7c6f1fd
Claus Prüfer <pruefer@webcodex.de>
2024-10-29 09:22:15 +0100

    Add WebApp.py for global test

---
+++ www/WebApp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import json
+
+class BaseClass:
+    def invoke(self, ReqJSON):
+        print('"'+ReqJSON+'"')
+        Request = json.loads(ReqJSON)
+        print(Request["payload"])
+        return Request["payload"]
+
