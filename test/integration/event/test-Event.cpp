e07fe2ed487ecac4d11bc5f088dfe7c872a7a498
Claus <pruefer@webcodex.de>
2025-09-20 12:47:08 +0200

    Remove unique_ptr, inspect later

--- test/integration/event/test-Event.cpp
+++ test/integration/event/test-Event.cpp
@@ -6,7 +6,7 @@
 
 BOOST_AUTO_TEST_CASE( test_event )
 {
-    auto pTest = std::make_unique<Callback>();
+    Callback* pTest = new Callback();
     pTest->checkEvent();
     pTest->callCallback();
 }
