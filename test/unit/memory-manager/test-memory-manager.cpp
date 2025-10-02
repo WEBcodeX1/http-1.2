e9881d2e5c942a3ec2a92830af7ffbc2ede11461
Claus Prüfer <pruefer@webcodex.de>
2025-10-02 19:54:59 +0200

    Fix according to comments, add check for segment reset

--- test/unit/memory-manager/test-memory-manager.cpp
+++ test/unit/memory-manager/test-memory-manager.cpp
@@ -27,27 +27,46 @@ BOOST_AUTO_TEST_CASE( test_basic_memory_manager_char_type )
 // 64bit (8 byte)
 BOOST_AUTO_TEST_CASE( test_basic_memory_manager_int64_type )
 {
-    MemoryManager<int64_t> BufferMemory(27, 13);
+    MemoryManager<int64_t> BufferMemory(27, 16);
     int64_t* BaseAddr = BufferMemory.getMemBaseAddress();
     int64_t* MemPointer;
     MemPointer = BufferMemory.getNextMemPointer();
-    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+(13*8)));
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+(16*8)));
     MemPointer = BufferMemory.getNextMemPointer();
-    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+2*(13*8)));
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+2*(16*8)));
     MemPointer = BufferMemory.getNextMemPointer();
-    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+3*(13*8)));
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+3*(16*8)));
 }
 
 // 16bit (2 byte)
 BOOST_AUTO_TEST_CASE( test_basic_memory_manager_short_type )
 {
-    MemoryManager<short> BufferMemory(120, 37);
+    MemoryManager<short> BufferMemory(120, 38);
     short* BaseAddr = BufferMemory.getMemBaseAddress();
     short* MemPointer;
     MemPointer = BufferMemory.getNextMemPointer();
-    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+(37*2)));
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+(38*2)));
     MemPointer = BufferMemory.getNextMemPointer();
-    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+2*(37*2)));
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+2*(38*2)));
     MemPointer = BufferMemory.getNextMemPointer();
-    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+3*(37*2)));
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+3*(38*2)));
+}
+
+// 64bit (8 byte) out of bounds check
+BOOST_AUTO_TEST_CASE( test_basic_memory_manager_int64_type_oob )
+{
+    MemoryManager<int64_t> BufferMemory(5, 24);
+    int64_t* BaseAddr = BufferMemory.getMemBaseAddress();
+    int64_t* MemPointer;
+    MemPointer = BufferMemory.getNextMemPointer();
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+(24*8)));
+    MemPointer = BufferMemory.getNextMemPointer();
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+2*(24*8)));
+    MemPointer = BufferMemory.getNextMemPointer();
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+3*(24*8)));
+    MemPointer = BufferMemory.getNextMemPointer();
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr+4*(24*8)));
+    MemPointer = BufferMemory.getNextMemPointer();
+    BOOST_CHECK_EQUAL(MemPointer, static_cast<void*>(BaseAddr));
+    MemPointer = BufferMemory.getNextMemPointer();
 }
