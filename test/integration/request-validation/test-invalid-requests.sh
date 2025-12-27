551e667dd76d65f96f19f89db2a8f76cc04e972d
Claus Prüfer <pruefer@webcodex.de>
2025-12-27 19:47:48 +0100

    Test invalid requests

---
+++ test/integration/request-validation/test-invalid-requests.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# test post request non-existing backend
+wget --post-data='{"payload": 536}' --header='Content-Type: application/json' http://testapp1.local/backend/xyz
+
+# test post request non-existing vhost
+# wget --post-data='{"payload": 812}' --header='Content-Type: application/json' http://testapp2.local/back/xyz
+
+# test get2post invalid parameters
+wget "http://testapp2.local/backend/endpoint2?param3=test1&param4=test2"
+
+# test get2post invalid backend endpoint
+# wget "http://testapp2.local/backend/endp1?par1=test1&par2=test2"
+
+# test get index (recursive content)
+wget -r "http://testapp1.local/nonexistent.html" >/dev/null 2>&1
+wget -r "http://testapp2.local/nonexistent.html" >/dev/null 2>&1
